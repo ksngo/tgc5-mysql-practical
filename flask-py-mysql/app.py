d8c48d166d413b5bff97de2cfc69bfdc49dc0990
ksngo <sevenelectrons@gmail.com>
2020-04-18 10:27:00 +0000

    practise qn2 18 april 20

--- flask-py-mysql/app.py
+++ flask-py-mysql/app.py
@@ -30,25 +30,46 @@ def show_employees(office_code):
 def display_employees():
 
     conn = data.get_connection('localhost', os.environ.get('C9_USER'), '', 'classicmodels')
-    user_input_fn = request.form.get('firstName')
-    user_input_ln = request.form.get('lastName')
-    cursor = dao.get_employees_filtered(conn, user_input_fn, user_input_ln)
-    
-    return render_template('employees_only.html', cursor=cursor)
 
-
-
-
-
-
-@app.route('/employees_filters')
-def employees_filters():
-    
-    conn = data.get_connection('localhost', os.environ.get('C9_USER'), '', 'classicmodels')
-    cursor = dao.get_employees_fn(conn)
+    # get the SQL data for selecting employees first name/last name at dropdown
+    cursor_fn = dao.get_employees_fn(conn)
     cursor_ln = dao.get_employees_ln(conn)
+    cursor_ct = dao.get_employees_ct(conn)
+    cursor_jt = dao.get_employees_jt(conn)
 
-    return render_template('employees_filters.html', cursor=cursor, cursor_ln = cursor_ln)
+    # get user input at dropdown for firstname and lastname when user submit via button
+    user_input_fn = ""
+    user_input_ln = ""
+    user_input_ct = ""
+    user_input_jt = ""
+
+    if request.method == 'POST':
+        user_input_fn = request.form.get('firstName')
+        user_input_ln = request.form.get('lastName')
+        user_input_ct = request.form.get('country')
+        user_input_jt = request.form.get('jobTitle')
+
+    # get SQL data for table with filters to display on employees_only.html
+    cursor = dao.get_employees_filtered(conn, user_input_fn, user_input_ln, user_input_ct, user_input_jt)
+    saved_list = list(cursor)
+    # print('--------hello------')
+    # print(saved_list)
+    
+
+    return render_template('employees_only.html', saved_list=saved_list, cursor_fn = cursor_fn , cursor_ln=cursor_ln, cursor_ct=cursor_ct, cursor_jt=cursor_jt)
+
+
+
+
+
+
+# @app.route('/employees_filters')
+# def employees_filters():
+    
+#     conn = data.get_connection('localhost', os.environ.get('C9_USER'), '', 'classicmodels')
+    
+
+#     return render_template('employees_filters.html', cursor=cursor, cursor_ln = cursor_ln)
     
     
 
--- flask-py-mysql/dao.py
+++ flask-py-mysql/dao.py
@@ -25,19 +25,37 @@ def get_employees_only(conn):
 
     return cursor
 
-def get_employees_filtered(conn, user_input_fn, user_input_ln):
+def get_employees_filtered(conn, user_input_fn, user_input_ln, user_input_ct, user_input_jt):
 
-    sql = "select * from `employees` where 1"
+
+
+    sql = f"""select * from `employees` 
+            join `offices` on `employees`.`officeCode` = `offices`.`officeCode`
+            where 1"""
     cursor = data.create_cursor(conn)
     
+    print('user_input_fn : ', user_input_fn)
     if user_input_fn != "":
         
         sql = sql + f" and `firstName`='{user_input_fn}' "
         print(sql)
 
+    print('user_input_ln : ', user_input_ln)
     if user_input_ln != "":
         sql = sql + f" and `lastName`='{user_input_ln}' "
 
+
+    print('user_input_ct :', user_input_ct)
+    if user_input_ct != "":
+        sql = sql + f" and `country`='{user_input_ct}' "
+
+    print('user_input_jt :', user_input_jt)
+    if user_input_jt !="":
+        sql = sql + f" and `jobTitle`='{user_input_jt}' "
+
+    
+    print('-------sql---------')
+    print(sql)
     cursor.execute(sql)
 
     return cursor
@@ -45,10 +63,10 @@ def get_employees_filtered(conn, user_input_fn, user_input_ln):
 def get_employees_fn(conn):
 
     sql = "select `firstName` from `employees` "
-    cursor = data.create_cursor(conn)
-    cursor.execute(sql)
+    cursor_fn = data.create_cursor(conn)
+    cursor_fn.execute(sql)
 
-    return cursor
+    return cursor_fn
 
 def get_employees_ln(conn):
 
@@ -57,3 +75,26 @@ def get_employees_ln(conn):
     cursor_ln.execute(sql)
 
     return cursor_ln
+
+
+def get_employees_ct(conn):
+
+    sql = f"""select `country` from `employees`
+            join `offices` on `employees`.`officeCode` =`offices`.`officeCode` """
+    
+    cursor_ct = data.create_cursor(conn)
+    cursor_ct.execute(sql)
+
+    cursor_ct = cursor_ct.fetchall()
+    print(cursor_ct)
+
+    return cursor_ct
+
+def get_employees_jt(conn):
+
+    sql = " select `jobTitle` from `employees` "
+
+    cursor_jt = data.create_cursor(conn)
+    cursor_jt.execute(sql)
+
+    return cursor_jt
